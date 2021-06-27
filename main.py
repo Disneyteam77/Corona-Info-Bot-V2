29e25b7a4789f265d7531c6056826e9fdcc19f1f
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-06-27 14:12:51 +0530

    Update

---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+FayasNoushad = Client(
+    "Corona-Info-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+API = "https://api.sumanjay.cf/covid/?country="
+
+START_TEXT = """
+Hello {}, I am a Disney Team Corona info telegram bot.
+
+Made by ❤ by [Doreamonfans](https://telegram.me/doreamonfans2)
+
+Send The Country Name Now And See Magic😁
+"""
+
+BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('👨‍🔧Join Updates Channel👨‍🔧', url='https://telegram.me/disneygrou'),
+        InlineKeyboardButton('🤖Support Group🤖', url='https://telegram.me/disneyteamchat')
+        ]]
+    )
+
+@FayasNoushad.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup,
+        quote=True
+    )
+
+@FayasNoushad.on_message(filters.private & filters.text)
+async def covid_info(bot, update):
+    try:
+        country = update.text.replace(" ", "+")
+        r = requests.get(API + country)
+        info = r.json()
+        country = info['country']
+        confirmed = info['confirmed']
+        active = info['active']
+        deaths = info['deaths']
+        info_id = info['id']
+        last_update = info['last_update']
+        latitude = info['latitude']
+        longitude = info['longitude']
+        recovered = info['recovered']
+        covid_info = f"""
+--**Covid 19 Information**--
+
+Country : `{country}`
+Actived : `{active}`
+Confirmed : `{confirmed}`
+Last Update : `{last_update}`
+ID : `{info_id}`
+Deaths : `{deaths}`
+Latitude : `{latitude}`
+Longitude : `{longitude}`
+Recovered : `{recovered}`
+
+Made by ❤ by [DoreamonFans](https://telegram.me/doreamonfans2)
+"""
+        await update.reply_text(
+            text=covid_info,
+            disable_web_page_preview=True,
+            quote=True,
+            reply_markup=BUTTONS
+        )
+    except Exception as error:
+        await update.reply_text(
+            text=error,
+            disable_web_page_preview=True,
+            quote=True
+        )
+
+Doreamonfans.run()
